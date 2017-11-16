350cac9f68d4c6302472ae2b5291edd4334dfb3f
Gianmarco <giorgian.g@gmail.com>
2017-11-16 18:12:45 -0500

    todomvc

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <HelloWorld/>
+    <TodoMVC/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import TodoMVC from './components/TodoMVC'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    TodoMVC
   }
 }
 </script>
---
+++ src/components/TodoMVC.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <input v-on:keyup.enter="addTask" v-model="newTask"/>
+    <div v-for="task in tasks" v-if="all || task.completed=== statusToShow ">
+      <input
+        v-model="task.completed"
+        type="checkbox"
+        :id="task.name"
+      />
+      <label :for="task.name">{{task.name}}</label>
+    </div>
+    <button v-on:click="all=true">all</button>
+    <button v-on:click="statusToShow=true; all=false">completed</button>
+    <button v-on:click="statusToShow= false; all=false">active</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoMVC',
+  data () {
+    return {
+      newTask: '',
+      tasks: [],
+      statusToShow: false,
+      all: true
+    }
+  },
+
+  methods: {
+    addTask () {
+      this.tasks.push({name: this.newTask, completed: false});
+      this.newTask = '';
+    },
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
